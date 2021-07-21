ec2b18ba30623195152bee9b7d7894e99bb883e8
hariboten <botegineer@outlook.com>
2021-07-21 23:18:01 +0900

    Add test for search

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -25,7 +25,22 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
-    return 3;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+        Rust:
+        safe, fast, productive.
+        Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive"], search(query, contents));
+    }
 }
