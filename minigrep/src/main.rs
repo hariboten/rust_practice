17c986173676cf99b13b67aeff810018f039ed46
hariboten <botegineer@outlook.com>
2021-07-21 22:20:37 +0900

    Use Result for run function

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process;
@@ -14,15 +15,20 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Applicationnn error: {}", e);
+
+        process::exit(1);
+    }
 }
 
-fn run(config: Config) {
+fn run(config: Config) -> Result<(), Box<Error>> {
     let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading file");
+    f.read_to_string(&mut contents)?;
+
+    Ok(())
 }
 
 struct Config {
